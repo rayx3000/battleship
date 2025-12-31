3c253b31cee30dde9821eaada6d5667075fe39e2
rayx3000 <candole2020@gmail.com>
2025-12-31 11:10:27 +0800

    add board tiles

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import './styles/general.css';
 import './styles/header.css';
 import './styles/main.css';
 
-
-
 function hello(){
     return "hello!";
 }
 
 const greet = hello();
 console.log(greet);
+
+const boards = document.querySelectorAll(".board");
+
+function createBoardGrid(){
+    boards.forEach(board => {
+        const boardGrids = 10**2;
+        board.style.gridTemplateColumns = `repeat(${Math.sqrt(boardGrids)}, 1fr)`;
+        board.style.gridTemplateRows = `repeat(${Math.sqrt(boardGrids)}, 1fr)`;
+        for(let i = 0; i < boardGrids; i++){
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            board.appendChild(tile);
+        }
+    });
+}
+
+createBoardGrid();
